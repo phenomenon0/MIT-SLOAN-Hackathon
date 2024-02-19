e1ba25cbb3f601984a424258c54285a771909cc5
Femi Adeniran <66181395+phenomenon0@users.noreply.github.com>
2024-02-18 20:38:25 -0600

    Create prompts.py

---
+++ Visual/prompts.py
@@ -0,0 +1,8 @@
+def Visualize(script):
+    '''
+    given a script in programming language create a  diagrams  to  visualizing  algorithm in 
+    ascii to a semi tech savvy audience -- 
+'''
+    return ascii_art 
+
+
